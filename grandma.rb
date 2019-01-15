dba0c530679969033d7e7d08ba2d4a5f751cc624
Steve Albonico <salbonico@gmail.com>
2019-01-15 16:08:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,11 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(input)
+
+if input == "I LOVE YOU GRANDMA!"
+then puts "I LOVE YOU TOO PUMPKIN!"
+
+end
