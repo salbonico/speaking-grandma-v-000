4d320627931b3c0647a43cce91bc4df8925353b6
Steve Albonico <salbonico@gmail.com>
2019-01-15 16:14:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(input)
 
 if input == "I LOVE YOU GRANDMA!"
 return "I LOVE YOU TOO PUMPKIN!"
-elsif input != input.uppercase
+elsif input != input.upcase
 return "HUH?! SPEAK UP, SONNY!"
 else
 return "NO, NOT SINCE 1938!"
